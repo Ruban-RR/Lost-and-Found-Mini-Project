eb69a5e118039207201d192220d3dce416fc91f4
RUBAN51 <rubanfamily6@gmail.com>
2024-02-24 19:25:40 +0530

    project

--- app/src/main/java/homepagemodule/LostForm.java
+++ app/src/main/java/homepagemodule/LostForm.java
@@ -30,15 +30,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class LostForm extends AppCompatActivity {
-    private String selectedValue,selectedplace,selectedheadphone,selectedWatch;
-    private LinearLayout llc;
-    private EditText BrandName, ModelName, ImeiNumber, ColorName, UniqueIn, ValuablesIn, dateoflost;
+    private String selectedValue,selectedplace,selectedheadphone,selectedWatch, selectedValuable;
+    private LinearLayout llc,llc2;
+    private EditText BrandName, ModelName, ImeiNumber, ColorName, UniqueIn, dateoflost, AadharName,AadharNumber,CardName,LicenseName, PANname,DOBinPAN,OtherThing, DOBinLicense;
     private Button  PostButton;
     private static final int PICK_IMAGE_REQUEST = 1;
-    private Spinner spinHead,spinWatch, spinPlace;
+    private Spinner spinHead,spinWatch, spinPlace, ValuablesIn, BankName;
     private DatabaseReference lostdb;
     private SharedPreferences cachefromlogin;
-    public static String brandname, modelname, imeinum, colorname, uniquefeature, valuabledetails, datelost,watchtype,headphonetype,locationname;
+    public static String brandname, modelname, imeinum, colorname, uniquefeature, datelost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +49,7 @@ public class LostForm extends AppCompatActivity {
 
         // Initialize layout elements after setting the content view
         llc = findViewById(R.id.linearLayoutContainer);
+        llc2= findViewById(R.id.linearLayoutContainer2);
         BrandName = findViewById(R.id.Brand);
         ModelName = findViewById(R.id.Model);
         ImeiNumber = findViewById(R.id.IMEI);
@@ -60,7 +61,17 @@ public class LostForm extends AppCompatActivity {
         spinWatch = findViewById(R.id.TypeWatch);
         spinPlace = findViewById(R.id.Place);
         dateoflost = findViewById(R.id.Date);
+        AadharName = findViewById(R.id.aadharname);
+        AadharNumber = findViewById(R.id.aadharnumber);
+        BankName = findViewById(R.id.bankname);
+        CardName = findViewById(R.id.nameincard);
+        LicenseName = findViewById(R.id.licensename);
+        PANname = findViewById(R.id.nameinpan);
+        DOBinPAN = findViewById(R.id.dobinpan);
+        OtherThing= findViewById(R.id.otherthing);
+        DOBinLicense = findViewById(R.id.licencedob);
         hideViews();
+        hideViews2();
         String[] Placesoflost = {"-Select Place-", "1", "2", "3"};
         ArrayAdapter<String> adapter4 = new ArrayAdapter<>(LostForm.this, android.R.layout.simple_spinner_item, Placesoflost);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -69,6 +80,76 @@ public class LostForm extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedplace = Placesoflost[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        String[] ValuablesInside = {"-Valuables Inside-", "Aadhar", "Bank Card", "Driving License","PAN Card","Others"};
+        ArrayAdapter<String> adapterValuable = new ArrayAdapter<>(LostForm.this, android.R.layout.simple_spinner_item, ValuablesInside);
+        adapterValuable.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ValuablesIn.setAdapter(adapterValuable);
+        ValuablesIn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedValuable = ValuablesInside[position];
+                if (selectedValuable.equals("-Valuables Inside-")){
+                    hideViews2();
+                } else if (selectedValue.equals("Aadhar")) {
+                    AadharName.setVisibility(View.VISIBLE);
+                    AadharNumber.setVisibility(View.VISIBLE);
+                    BankName.setVisibility(View.GONE);
+                    CardName.setVisibility(View.GONE);
+                    LicenseName.setVisibility(View.GONE);
+                    DOBinPAN.setVisibility(View.GONE);
+                    PANname.setVisibility(View.GONE);
+                    DOBinLicense.setVisibility(View.GONE);
+                    OtherThing.setVisibility(View.GONE);
+                } else if (selectedValue.equals("Bank Card")) {
+                    AadharName.setVisibility(View.GONE);
+                    AadharNumber.setVisibility(View.GONE);
+                    BankName.setVisibility(View.VISIBLE);
+                    CardName.setVisibility(View.VISIBLE);
+                    LicenseName.setVisibility(View.GONE);
+                    DOBinPAN.setVisibility(View.GONE);
+                    PANname.setVisibility(View.GONE);
+                    DOBinLicense.setVisibility(View.GONE);
+                    OtherThing.setVisibility(View.GONE);
+                } else if (selectedValue.equals("Driving License")) {
+                    AadharName.setVisibility(View.GONE);
+                    AadharNumber.setVisibility(View.GONE);
+                    BankName.setVisibility(View.GONE);
+                    CardName.setVisibility(View.GONE);
+                    LicenseName.setVisibility(View.VISIBLE);
+                    DOBinPAN.setVisibility(View.GONE);
+                    PANname.setVisibility(View.GONE);
+                    DOBinLicense.setVisibility(View.VISIBLE);
+                    OtherThing.setVisibility(View.GONE);
+                } else if (selectedValue.equals("PAN Card")) {
+                    AadharName.setVisibility(View.GONE);
+                    AadharNumber.setVisibility(View.GONE);
+                    BankName.setVisibility(View.GONE);
+                    CardName.setVisibility(View.GONE);
+                    LicenseName.setVisibility(View.GONE);
+                    DOBinPAN.setVisibility(View.VISIBLE);
+                    PANname.setVisibility(View.VISIBLE);
+                    DOBinLicense.setVisibility(View.GONE);
+                    OtherThing.setVisibility(View.GONE);
+                } else if (selectedValue.equals("Others")) {
+                    AadharName.setVisibility(View.GONE);
+                    AadharNumber.setVisibility(View.GONE);
+                    BankName.setVisibility(View.GONE);
+                    CardName.setVisibility(View.GONE);
+                    LicenseName.setVisibility(View.GONE);
+                    DOBinLicense.setVisibility(View.GONE);
+                    DOBinPAN.setVisibility(View.GONE);
+                    PANname.setVisibility(View.GONE);
+                    OtherThing.setVisibility(View.VISIBLE);
+                }else {
+                    llc2.setVisibility(View.GONE);
+                }
             }
 
             @Override
@@ -135,8 +216,6 @@ public class LostForm extends AppCompatActivity {
                         ValuablesIn.setVisibility(View.GONE);
                         spinHead.setVisibility(View.GONE);
                         spinWatch.setVisibility(View.GONE);
-
-
                     } else if (selectedValue.equals("Bag")) {
                         llc.setVisibility(View.VISIBLE);
                         BrandName.setVisibility(View.VISIBLE);
@@ -149,7 +228,6 @@ public class LostForm extends AppCompatActivity {
                         spinHead.setVisibility(View.GONE);
                         dateoflost.setVisibility((View.VISIBLE));
                         spinWatch.setVisibility(View.GONE);
-
                     }
                     else if (selectedValue.equals("Watch")) {
                         llc.setVisibility(View.VISIBLE);
@@ -163,8 +241,6 @@ public class LostForm extends AppCompatActivity {
                         spinPlace.setVisibility(View.VISIBLE);
                         spinWatch.setVisibility(View.VISIBLE);
                         dateoflost.setVisibility((View.VISIBLE));
-
-
                     }
                     else if (selectedValue.equals("Purse")) {
                         llc.setVisibility(View.VISIBLE);
@@ -179,7 +255,6 @@ public class LostForm extends AppCompatActivity {
                         spinWatch.setVisibility(View.GONE);
                         dateoflost.setVisibility((View.VISIBLE));
 
-
                     }
                     else if (selectedValue.equals("Headphones")) {
                         llc.setVisibility(View.VISIBLE);
@@ -193,7 +268,6 @@ public class LostForm extends AppCompatActivity {
                         spinHead.setVisibility(View.VISIBLE);
                         spinWatch.setVisibility(View.GONE);
                         dateoflost.setVisibility((View.VISIBLE));
-
                     }else {
                         // For other items, hide all EditText views
                         llc.setVisibility(View.GONE);
@@ -230,13 +304,24 @@ public class LostForm extends AppCompatActivity {
         spinPlace.setVisibility(View.GONE);
         dateoflost.setVisibility((View.GONE));
     }
+    private void hideViews2(){
+        AadharName.setVisibility(View.GONE);
+        AadharNumber.setVisibility(View.GONE);
+        BankName.setVisibility(View.GONE);
+        CardName.setVisibility(View.GONE);
+        LicenseName.setVisibility(View.GONE);
+        DOBinPAN.setVisibility(View.GONE);
+        DOBinLicense.setVisibility(View.GONE);
+        PANname.setVisibility(View.GONE);
+        OtherThing.setVisibility(View.GONE);
+    }
+
         private void postToDB(){
             brandname = BrandName.getText().toString();
             modelname = ModelName.getText().toString();
             imeinum = ImeiNumber.getText().toString();
             colorname = ColorName.getText().toString();
             uniquefeature = UniqueIn.getText().toString();
-            valuabledetails = ValuablesIn.getText().toString();
             datelost = dateoflost.getText().toString();
 
 
@@ -251,7 +336,7 @@ public class LostForm extends AppCompatActivity {
                     lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("IMEI Number").setValue(imeinum);
                     lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("Color").setValue(colorname);
                     lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("Uniqueness").setValue(uniquefeature);
-                    lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("ValuablesInside").setValue(valuabledetails);
+                    lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("ValuablesInside").setValue(selectedValuable);
                     lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("Date").setValue(datelost);
                     lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("Watch Type").setValue(selectedWatch);
                     lostdb.child("users").child(registerNum).child("lost").child(selectedValue).child("Headphone Type").setValue(selectedheadphone);
